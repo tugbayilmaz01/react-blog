509f1570581ed509472dde13c1420582c3ba13dc
tugbayilmaz01 <tugbayill01@gmail.com>
2022-10-13 15:52:34 +0300

    homepage background

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,5 +1,19 @@
+import Blog from "./Blog";
+import  blogphoto from "../assets/blogphoto.jpg"; 
+
 function BlogDetail(){
-    return
+    return (
+        <div style={{ backgroundImage:`url(${blogphoto})`,
+      backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh',
+        marginTop:"0px"}}>
+        
+          <h2 style={{textAlign:"center", letterSpacing:"3px", padding:"20rem", paddingRight:"1px"}}>Welcome!</h2> 
+      </div>
+    );
 }
 
 export default BlogDetail;
